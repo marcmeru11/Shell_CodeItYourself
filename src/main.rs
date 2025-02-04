a273a3b71acd48c766db6bf4ada8a572200eebf1
marcmeru <marcmeru@gmail.com>
2025-02-04 11:24:31 +0100

    type exe

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
+use pathsearch::find_executable_in_path;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 use std::process::exit;
+
+const BUILTINS: [&str; 3] = ["exit", "echo", "type"];
+
+fn run_exit(args: Vec<&str>) {
+    let code = args.get(0).map_or(0, |c| c.parse().unwrap_or(0));
+    std::process::exit(code);
+}
+
+fn run_echo(args: Vec<&str>) {
+    println!("{}", args.join(" "));
+}
+
+fn run_type(args: Vec<&str>) {
+    let arg = args.get(0).unwrap();
+    if BUILTINS.contains(arg) {
+        println!("{} is a shell builtin", arg);
+    } else if let Some(exe) = find_executable_in_path(arg) {
+        println!("{} is {}",arg, exe.display());
+    } else {
+        println!("{} not found", arg);
+    }
+}
 
 fn main() {
     loop {
@@ -14,30 +37,19 @@ fn main() {
 
         let command = input.trim().split_whitespace();
         let mut args = command.clone().skip(1);
-
         match command.clone().next() {
-            Some("exit") => exit(args.next().unwrap_or("0").parse().unwrap()),
+            Some("exit") => run_exit(args.collect()),
             Some("echo") => {
-                println!("{}", args.collect::<Vec<&str>>().join(" "));
+                run_echo(args.collect());
                 continue;
             }
             Some("type") => {
-                let arg = args.next().unwrap();
-                match arg {
-                    "exit" | "echo" | "type" => {
-                        println!("{} is a shell builtin", arg);
-                    }
-                    _ => {
-                        println!("{}: not found", arg);
-                    }
-
-                }
+                run_type(args.collect());
                 continue;
             }
             _ => {}
-            
         }
 
-        println!("{}: command not found", input.trim())
+        println!("{}: command not found", input.trim());
     }
 }
