87a9c56a56e0f652eed13319eed0b652921b11e5
marcmeru <marcmeru@gmail.com>
2025-02-04 10:57:15 +0100

    exit builtin

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::exit;
 
 fn main() {
     loop {
@@ -12,6 +13,19 @@ fn main() {
         stdin.read_line(&mut input).unwrap();
 
         let command = input.trim().split_whitespace();
+
+        match command.clone().next() {
+            Some("exit") => exit(input.trim().split_whitespace().nth(1).unwrap_or("0").parse().unwrap()),
+            /*
+            Some("echo") => {
+                let args = command.skip(1);
+                println!("{}", args.collect::<Vec<&str>>().join(" "));
+                continue;
+            }
+            */
+            _ => {}
+            
+        }
 
         println!("{}: command not found", input.trim())
     }
