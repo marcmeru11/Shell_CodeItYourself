d5961e87efc3c1a382780232593547f6cc3b367f
MarcMeRu11 <marcmeru@gmail.com>
2025-02-07 13:58:11 +0100

    backslash with quotes try 25

--- src/utils.rs
+++ src/utils.rs
@@ -9,49 +9,46 @@ pub fn split_command(input: &str) -> Vec<String> {
 
     while let Some(c) = iter.next() {
         match c {
-            '\'' => {
-                if in_single_quote {
-                    in_single_quote = false;
-                } else if !in_double_quote {
-                    in_single_quote = true;
-                } else {
-                    word.push(c);
-                }
+            '\'' if !in_double_quote => {
+                in_single_quote = !in_single_quote;
+                continue;
             }
-
-            '"' => {
-                if in_double_quote {
-                    in_double_quote = false;
-                } else if !in_single_quote {
-                    in_double_quote = true;
-                } else {
-                    word.push(c);
-                }
+            '"' if !in_single_quote => {
+                in_double_quote = !in_double_quote;
+                continue;
             }
+            '\\' if !in_single_quote && !in_double_quote => {
+                let c = iter.next().unwrap();
 
-            '\\' => {
-                if in_single_quote || (in_double_quote && iter.peek() == Some(&'"')) {
-                    word.push(c);
-                } else if iter.peek() == Some(&'\\') || iter.peek() == Some(&'$') {
-                    word.push(iter.next().unwrap());
-                } else {
-                    word.push(c);
-                }
+                word.push(c);
+                continue;
             }
+            '\\' if in_double_quote => {
+                match iter.peek().unwrap() {
+                    '\\' | '$' | '"' => {
+                        word.push(iter.next().unwrap());
+                    }
 
-            ' ' => {
-                if in_single_quote || in_double_quote {
-                    word.push(c);
-                } else if !word.is_empty() {
-                    result.push(word);
-                    word = String::new();
-                }
+                    _ => word.push(c),
+                };
+                continue;
             }
-
+            ' ' if !in_single_quote && !in_double_quote => {
+                if !word.is_empty() {
+                    result.push(word.clone());
+                    word.clear();
+                }
+                continue;
+            }
             _ => {
                 word.push(c);
             }
         }
     }
+
+    if !word.is_empty() {
+        result.push(word);
+    }
+
     result
 }
