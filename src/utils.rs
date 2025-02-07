9f38ea4e82931be4261ff5b850186264c23d0cb0
MarcMeRu11 <marcmeru@gmail.com>
2025-02-07 12:40:10 +0100

    Backslash with quotes try 6

--- src/utils.rs
+++ src/utils.rs
@@ -6,12 +6,23 @@ pub fn split_command(input: &str) -> Vec<String> {
     let mut in_double_quotes = false;
     let mut chars = input.chars().peekable();
 
-    while let Some(c) = chars.next() { 
-        //Enclosing backslashes within double quotes " preserves the special meaning of the backslash, only when it is followed by \, $, " or newline. 
+    while let Some(c) = chars.next() {
+        //Enclosing backslashes within double quotes " preserves the special meaning of the backslash, only when it is followed by \, $, " or newline.
         match c {
             '\\' if !in_single_quotes && !in_double_quotes => {
-                let c = chars.next().unwrap();
-                word.push(c);
+                if let Some(&next_char) = chars.peek() {
+                    match next_char {
+                        '\\' | '$' | '"' | '\n' => {
+                            let c = chars.next().unwrap();
+                            word.push(c);
+                        }
+                        _ => {
+                            word.push(c);
+                        }
+                    }
+                } else {
+                    word.push(c);
+                }
             }
             '\'' if !in_double_quotes => {
                 in_single_quotes = !in_single_quotes;
